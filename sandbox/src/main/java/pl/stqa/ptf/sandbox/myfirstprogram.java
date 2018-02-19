ff1f78ff7bde7722f0ca8b9cf824577da2f21286
mariushu <36562086+mariushu@users.noreply.github.com>
2018-02-19 22:52:35 +0100

    10. Zmienne i wartości

--- sandbox/src/main/java/pl/stqa/ptf/sandbox/myfirstprogram.java
+++ sandbox/src/main/java/pl/stqa/ptf/sandbox/myfirstprogram.java
@@ -1,9 +1,12 @@
 package pl.stqa.ptf.sandbox;
 
-public class Myfirstprogram {
+public class myfirstprogram {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        int l = 8;
+        int s = l * l;
+        System.out.println("Płaszczyzna kwadratu " + l + " = " + s);
     }
 
 }	
