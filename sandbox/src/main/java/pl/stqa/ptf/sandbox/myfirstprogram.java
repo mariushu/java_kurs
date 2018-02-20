f39009d3f4778a836a9fbaa8de5457deda890738
mariushu <36562086+mariushu@users.noreply.github.com>
2018-02-20 21:07:03 +0100

    13. Funkcje

--- sandbox/src/main/java/pl/stqa/ptf/sandbox/myfirstprogram.java
+++ sandbox/src/main/java/pl/stqa/ptf/sandbox/myfirstprogram.java
@@ -3,10 +3,30 @@ package pl.stqa.ptf.sandbox;
 public class myfirstprogram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
-        int l = 8;
-        int s = l * l;
-        System.out.println("Płaszczyzna kwadratu " + l + " = " + s);
+        hello("dddddd");
+        hello("d");
+        hello("d");
+
+        double l = 5;
+        System.out.println("Powierzchnia kwadratu o boku " + l + " = " + area(l));
+
+        double a = 4, b = 3;
+        System.out.println("Powierzchnia prostokonta o bokach " + a + " i " + b + " = " + area(a,b));
+    }
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, "+ somebody + "!");
+
+    }
+    public static double area (double l) {
+        return l * l;
+
+    }
+    public static double area (double a, double b ) {
+        return a * b;
+
     }
 
-}	
+
+}
+
