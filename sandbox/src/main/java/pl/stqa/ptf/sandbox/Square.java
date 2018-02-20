446792fd389d5c9f141b64c4e24245cc5e252fc2
mariushu <36562086+mariushu@users.noreply.github.com>
2018-02-20 22:34:29 +0100

    15. Metody: funkcje łaczone z obiektami

--- sandbox/src/main/java/pl/stqa/ptf/sandbox/Square.java
+++ sandbox/src/main/java/pl/stqa/ptf/sandbox/Square.java
@@ -2,7 +2,11 @@ package pl.stqa.ptf.sandbox;
 
 public class Square {
   public double l;
+
   public Square (double len){
     this.l = len;
+  }
+  public double area () {
+    return this.l * this.l;
   }
 }
